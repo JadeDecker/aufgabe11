cf9ebc71f7bdab048af66a669f64a887e6b60532
jade <jade@Jade>
2017-07-02 17:52:38 +0200

    Heroku Verbindung

--- DatabaseClient.js
+++ DatabaseClient.js
@@ -34,8 +34,8 @@ var DatabaseClient;
     }
     function sendRequest(_query, _callback) {
         let xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://localhost:8100?" + _query, true);
-        //xhr.open("GET", "https://eia2-servertest.herokuapp.com?color=" + _color, true);
+        //xhr.open("GET", "http://localhost:8100?" + _query, true);
+        xhr.open("GET", "https://eia-aufgabe11.herokuapp.com/?" + _query, true);
         xhr.addEventListener("readystatechange", _callback);
         xhr.send();
     }
